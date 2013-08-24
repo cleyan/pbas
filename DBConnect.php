0f11df3634a0a37fcdb1910fa3a283a029a35102
Rahul-Sagore <rahul.sagore@gmail.com>
2013-08-25 00:47:35 +0530

    Deleted DBConnect file

--- DBConnect.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-	$con = mysqli_connect('localhost','root','root','pbas_db');
-	if(mysqli_connect_errno($con)){
-		echo 'Failed to connect to the database : '.mysqli_connect_error();
-		die();
-	}
-
-?>
