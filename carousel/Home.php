467ddd84e4098a1e5ff9f7c9af841a160f1a4219
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2013-08-28 18:24:17 +0530

    Added ob_start() function at the top of the page

--- carousel/Home.php
+++ carousel/Home.php
@@ -1,6 +1,8 @@
+<?php ob_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 	session_start();
+	
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -14,7 +16,6 @@
  	}
 </style>
 </head>
-
 <body>
 <?php 
 	if(isset($_SESSION['username'])){
