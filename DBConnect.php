cf1fa39b46e21cd983b8f58c005d0b661a014efe
root <root@nitesh007-Lenovo-B480.(none)>
2014-02-17 09:23:50 +0530

    change the mysql password in database connection file

--- DBConnect.php
+++ DBConnect.php
@@ -1,5 +1,5 @@
 <?PHP
-	$con = mysqli_connect('localhost','root','','pbas_db');
+	$con = mysqli_connect('localhost','root','root','pbas_db');
 	if(mysqli_connect_errno($con)){
 		echo 'Failed to connect to the database : '.mysqli_connect_error();
 		die();
